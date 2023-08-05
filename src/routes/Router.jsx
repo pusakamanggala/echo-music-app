528342ae6819459e6c3a1d26158951207259c39b
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-05 16:18:38 +0700

    update : get authentication function

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,33 +1,16 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayout from "../layout/DashboardLayout";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import PageNotFound from "../pages/PageNotFound";
-import AuthContext from "../context/AuthProvider";
 import ExploreFeaturedAndNewRelease from "../pages/ExploreFeaturedAndNewRelease";
+import { getAccessTokenFromCookie } from "../utils/helpers";
 
 const Router = () => {
-  const { setAccessToken } = useContext(AuthContext);
-
   const isAuthenticated = () => {
-    // Get all cookies as a string
-    const cookies = document.cookie;
-
-    // Split the cookies string into individual cookie key-value pairs
-    const cookieArray = cookies.split(";");
-
-    // Iterate over the cookieArray to check if the "access_token" cookie exists
-    for (let i = 0; i < cookieArray.length; i++) {
-      const cookie = cookieArray[i].trim();
-
-      // Check if the cookie name matches "access_token"
-      if (cookie.startsWith("access_token=")) {
-        return true; // "access_token" cookie found, user is authenticated
-      }
-    }
-
-    return false; // "access_token" cookie not found, user is not authenticated
+    const accessToken = getAccessTokenFromCookie();
+    return accessToken !== null; // If accessToken is not null, user is authenticated
   };
 
   const isLogin = isAuthenticated();

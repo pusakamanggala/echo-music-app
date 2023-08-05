fa2fab7ea0d4528d6b7c980b6246e6415f755c50
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-05 15:37:13 +0700

    add : 404 page

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold ">404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+};
+
+export default PageNotFound;

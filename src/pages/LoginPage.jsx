4ca8dd3128c1797f815a1c5c25df72666575098a
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-05 15:37:07 +0700

    add : login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import useLogin from "../hooks/useLogin";
+
+const LoginPage = () => {
+  const clientId = "4c028baed0074747ab50de3ba9ac86b9";
+  const clientSecret = "725be5fa28b749febd2b39ff7b971bc5";
+
+  const login = useLogin(clientId, clientSecret);
+
+  const handleLogin = () => {
+    login.mutate();
+  };
+
+  if (login.isSuccess) {
+    // set the access token to cookie with 1 hour expiration
+    document.cookie = `access_token=${login.data.access_token};max-age=3600`;
+    // reload the page
+    window.location.reload();
+  }
+  return (
+    <div className="h-screen bg-sky-600 flex justify-center items-center">
+      <button
+        className="text-white p-3 rounded-lg bg-green-600 shadow-md"
+        onClick={handleLogin}
+      >
+        Login with Spotify
+      </button>
+    </div>
+  );
+};
+
+export default LoginPage;

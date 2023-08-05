259f8c76ee0394ead9c8dbd52b2ce43936992d43
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-05 15:37:00 +0700

    add : consume api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,43 +1,130 @@
-import React from "react";
-import bannerImg from "../img/banner.jpg";
+import React, { useEffect, useState } from "react";
+import useFetchFeaturedPlaylists from "../hooks/useFecthFeaturedPLaylists";
+import useFetchNewReleaseAlbum from "../hooks/useFetchNewReleaseAlbum";
+import {
+  getAccessTokenFromCookie,
+  getFetchLimitByScreen,
+} from "../utils/helpers";
+import LoadingAnimation from "../img/LoadingAnimation.gif";
 
 const HomePage = () => {
-  const topSongs = [
-    { id: 1, title: "Song 1", artist: "Artist 1", image: "song1.jpg" },
-    { id: 2, title: "Song 2", artist: "Artist 2", image: "song2.jpg" },
-    { id: 3, title: "Song 3", artist: "Artist 3", image: "song3.jpg" },
-    { id: 4, title: "Song 3", artist: "Artist 3", image: "song3.jpg" },
-    { id: 5, title: "Song 3", artist: "Artist 3", image: "song3.jpg" },
-    // Add more songs as needed
-  ];
+  const accessToken = getAccessTokenFromCookie();
+  const [fetchLimit, setFetchLimit] = useState();
+
+  useEffect(() => {
+    const updateFetchLimit = () => {
+      setFetchLimit(getFetchLimitByScreen());
+    };
+
+    // Initial setup on component mount
+    updateFetchLimit();
+
+    // Event listener for screen size changes
+    window.addEventListener("resize", updateFetchLimit);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", updateFetchLimit);
+    };
+  }, []);
+
+  const {
+    data: newAlbumData,
+    isLoading: isNewAlbumLoading,
+    isError: isNewAlbumError,
+    isSuccess: isNewAlbumSuccess,
+  } = useFetchNewReleaseAlbum({
+    accessToken,
+    limit: fetchLimit,
+  });
+
+  const {
+    data: featuredPlaylistsData,
+    isLoading: isFeaturedPlaylistsLoading,
+    isError: isFeaturedPlaylistsError,
+    isSuccess: isFeaturedPlaylistsSuccess,
+  } = useFetchFeaturedPlaylists({ accessToken, limit: fetchLimit });
 
   return (
     <div>
       {/* Banner */}
       <div className="bg-black h-96 rounded-lg text-white overflow-hidden relative w-full">
         <img
-          src={bannerImg}
+          src="https://c4.wallpaperflare.com/wallpaper/1021/802/327/musicians-freddie-mercury-freddy-mercury-brian-may-roger-taylor-john-deacon-men-queen-music-band-black-background-album-covers-bohemian-rhapsody-wallpaper-preview.jpg"
           alt="banner"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-contain"
         />
       </div>
-      {/* New Releases */}
+      {/* featured PLaylist */}
       <div className="flex justify-between items-center text-white">
-        <h1 className="my-8 text-xl font-semibold">New Releases</h1>
+        <h1 className="my-8 text-xl font-bold">Today Featured Playlist</h1>
         <h1 className="font-semibold">Show all</h1>
       </div>
-      <div className="grid grid-cols-5 gap-4 ">
-        {topSongs.map((song) => (
-          <div key={song.id} className="bg-black rounded-lg p-4 shadow-md ">
-            <img
-              src={song.image}
-              alt={song.title}
-              className="w-full mb-2 rounded-md"
-            />
-            <h3 className="text-lg font-semibold">{song.title}</h3>
-            <p className="text-gray-500">{song.artist}</p>
+      {isFeaturedPlaylistsLoading && (
+        <img className="mx-auto w-28" src={LoadingAnimation} alt="" />
+      )}
+      {isFeaturedPlaylistsError && (
+        <h1 className="font-semibold text-center text-white">
+          Something went wrong, please try again
+        </h1>
+      )}
+      {isFeaturedPlaylistsSuccess && (
+        <>
+          <div className="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+            {featuredPlaylistsData.map((playlist) => (
+              <div
+                key={playlist.id}
+                className="bg-white/20 backdrop-filter rounded-lg p-4 shadow-md text-white hover:bg-white/40 transition-colors duration-500 ease-in-out cursor-pointer"
+              >
+                <img
+                  src={playlist.images[0].url}
+                  alt={playlist.name}
+                  className="w-full mb-2 rounded-md"
+                />
+                <h3 className="text-lg font-semibold line-clamp-1">
+                  {playlist.name}
+                </h3>
+                <p className="line-clamp-2 font-medium text-gray-300">
+                  {playlist.description}
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
+        </>
+      )}
+      {/* New Releases */}
+      <div className="flex justify-between items-center text-white">
+        <h1 className="my-8 text-xl font-bold">New Releases</h1>
+        <h1 className="font-semibold">Show all</h1>
+      </div>
+      {isNewAlbumLoading && (
+        <img className="mx-auto w-28" src={LoadingAnimation} alt="" />
+      )}
+      {isNewAlbumError && (
+        <h1 className="font-semibold text-center text-white">
+          Something went wrong, please try again
+        </h1>
+      )}
+      <div className="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 ">
+        {isNewAlbumSuccess &&
+          newAlbumData.albums.items.map((album) => (
+            <div
+              key={album.id}
+              className="bg-white/20 backdrop-filter rounded-lg p-4 shadow-md text-white hover:bg-white/40 transition-colors duration-500 ease-in-out cursor-pointer"
+            >
+              <img
+                src={album.images[1].url}
+                alt={album.name}
+                className="w-full mb-2 rounded-md"
+              />
+              <h3 className="text-lg font-semibold line-clamp-1">
+                {album.name}
+              </h3>
+              <p className="line-clamp-2 font-medium text-gray-300">
+                {album.artists[0].name}
+              </p>
+            </div>
+          ))}
       </div>
     </div>
   );

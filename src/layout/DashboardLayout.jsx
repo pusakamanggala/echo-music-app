7899b26ed126ae8b8a12c821a5427fcc7619e3d6
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-09 14:44:57 +0700

    update : size

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -6,7 +6,7 @@ const DashboardLayout = ({ children }) => {
     <div className="flex h-screen bg-black p-4">
       {/* Sidebar component */}
       <Sidebar />
-      <div className="flex-grow bg-gradient-to-t from-black to-sky-700 rounded-lg p-4 overflow-y-scroll">
+      <div className="flex-grow bg-gradient-to-t from-black to-sky-700 rounded-lg p-4 overflow-y-scroll h-full">
         {/* Content */}
         {/* Main content */}
         {children}

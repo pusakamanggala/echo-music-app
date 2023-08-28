77a8cb1c349a05260b52523506c1eaec056536b0
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-28 15:22:02 +0700

    add : safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["grid-cols-3", "grid-cols-4", "grid-cols-5", "grid-cols-6"], // always include these classes
   theme: {
     extend: {},
   },

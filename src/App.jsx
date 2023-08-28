3b922b76a1980e7125bc7a8f23aa59a8115c7bb4
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-28 15:22:30 +0700

    add : dont refecth on window focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./routes/Router";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // Disable refetching on window focus
+    },
+  },
+});
 
 const App = () => {
   return (

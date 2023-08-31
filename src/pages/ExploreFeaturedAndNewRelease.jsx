f902bbcfa5971643dc98b0ceabfec584e243860c
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-31 15:21:22 +0700

    add : navigate to playlist and album details

--- src/pages/ExploreFeaturedAndNewRelease.jsx
+++ src/pages/ExploreFeaturedAndNewRelease.jsx
@@ -4,11 +4,15 @@ import { getAccessTokenFromCookie } from "../utils/helpers";
 import { useParams } from "react-router-dom";
 import LoadingAnimation from "../img/loadingAnimation.gif";
 import useGetFetchLimit from "../hooks/useGetFecthLimit";
+import useNavigatePlaylistDetails from "../hooks/useNavigatePlaylistDetails";
+import useNavigateToAlbumDetails from "../hooks/useNavigateToAlbumDetails";
 
 const ExploreFeaturedPlaylist = () => {
   const { type } = useParams();
   const accessToken = getAccessTokenFromCookie();
   const { gridSize } = useGetFetchLimit();
+  const navigateAlbumDetails = useNavigateToAlbumDetails();
+  const navigatePlaylistDetails = useNavigatePlaylistDetails();
 
   const fetchHook =
     type === "new-release"
@@ -40,6 +44,7 @@ const ExploreFeaturedPlaylist = () => {
               data.map((playlist) => (
                 <div
                   key={playlist.id}
+                  onClick={() => navigatePlaylistDetails(playlist.id)}
                   className="bg-white/20 backdrop-filter rounded-lg p-4 shadow-md text-white hover:bg-white/40 transition-colors duration-500 ease-in-out cursor-pointer"
                 >
                   <img
@@ -59,6 +64,7 @@ const ExploreFeaturedPlaylist = () => {
               data.albums.items.map((album) => (
                 <div
                   key={album.id}
+                  onClick={() => navigateAlbumDetails(album.id)}
                   className="bg-white/20 backdrop-filter rounded-lg p-4 shadow-md text-white hover:bg-white/40 transition-colors duration-500 ease-in-out cursor-pointer"
                 >
                   <img

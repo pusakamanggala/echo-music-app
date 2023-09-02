c829a5884c8d0f12007e492b154d3d8ba3ba27ed
Pusaka Manggala <pusakamanggala@gmail.com>
2023-09-02 16:40:23 +0700

    add : close live view when navigating in mobile

--- src/hooks/useNavigateToAlbumDetails.jsx
+++ src/hooks/useNavigateToAlbumDetails.jsx
@@ -1,10 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import NowPlayingContext from "../context/NowPlayingProvider";
+import useIsMobile from "./useIsMobile";
 
 const useNavigateToAlbumDetails = () => {
   const navigate = useNavigate();
+  const { setPlayingView } = useContext(NowPlayingContext);
+  const isMobile = useIsMobile();
 
   const navigateToAlbumDetails = (albumId) => {
     navigate(`/album/${albumId}`);
+    if (isMobile) setPlayingView(false);
   };
 
   return navigateToAlbumDetails;
--- src/hooks/useNavigateToArtistDetails.jsx
+++ src/hooks/useNavigateToArtistDetails.jsx
@@ -1,10 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import NowPlayingContext from "../context/NowPlayingProvider";
+import useIsMobile from "./useIsMobile";
 
 const useNavigateToArtistDetails = () => {
   const navigate = useNavigate();
+  const { setPlayingView } = useContext(NowPlayingContext);
+  const isMobile = useIsMobile();
 
   const navigateToArtistDetails = (artistId) => {
     navigate(`/artist/${artistId}`);
+    if (isMobile) setPlayingView(false);
   };
 
   return navigateToArtistDetails;

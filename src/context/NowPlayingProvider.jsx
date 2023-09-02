e826d41815f80f1baf9537d638b2ebb69c9a3d96
Pusaka Manggala <pusakamanggala@gmail.com>
2023-09-02 16:43:25 +0700

    add : not open live view when play track in mobile

--- src/context/NowPlayingProvider.jsx
+++ src/context/NowPlayingProvider.jsx
@@ -1,9 +1,11 @@
 import { createContext, useState, useEffect } from "react";
 import PropType from "prop-types";
+import useIsMobile from "../hooks/useIsMobile";
 
 const NowPlayingContext = createContext();
 
 export const NowPlayingProvider = ({ children }) => {
+  const isMobile = useIsMobile();
   const [nowPlaying, setNowPlaying] = useState(() => {
     const storedNowPlaying = sessionStorage.getItem("nowPlaying");
     return storedNowPlaying || "";
@@ -17,7 +19,7 @@ export const NowPlayingProvider = ({ children }) => {
 
   const handlePlayTrack = (trackId) => {
     setNowPlaying(trackId);
-    setPlayingView(true);
+    if (!isMobile) setPlayingView(true);
   };
 
   return (

452920cdf6808daca7905500184958beead1f070
Pusaka Manggala <pusakamanggala@gmail.com>
2023-09-02 15:23:00 +0700

    add : set playing view to false when nav pressed

--- src/components/MobileNavBar.jsx
+++ src/components/MobileNavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
@@ -6,9 +6,11 @@ import {
   faCircleExclamation,
 } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+import NowPlayingContext from "../context/NowPlayingProvider";
 
 const MobileNavBar = () => {
   const [showNote, setShowNote] = useState(false);
+  const { setPlayingView } = useContext(NowPlayingContext);
   return (
     <nav className="grid grid-cols-3 items-center p-5 bg-slate-900">
       <NavLink
@@ -19,6 +21,7 @@ const MobileNavBar = () => {
             ? "text-white flex flex-col justify-center items-center"
             : "text-gray-500 flex flex-col justify-center items-center"
         }
+        onClick={() => setPlayingView(false)}
       >
         <FontAwesomeIcon className="h-6" icon={faMagnifyingGlass} />
         <p className="text-xs font-semibold">Search</p>
@@ -31,6 +34,7 @@ const MobileNavBar = () => {
             ? "text-white flex flex-col justify-center items-center"
             : "text-gray-500 flex flex-col justify-center items-center"
         }
+        onClick={() => setPlayingView(false)}
       >
         <FontAwesomeIcon className="h-6" icon={faHouse} />
         <p className="text-xs font-semibold">Home</p>

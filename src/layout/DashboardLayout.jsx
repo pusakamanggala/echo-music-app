a4ae4f5b75820aad72bf2b970d9a3cf2614fe860
Pusaka Manggala <pusakamanggala@gmail.com>
2023-09-02 15:09:09 +0700

    add : mobile nav bar

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -4,37 +4,44 @@ import SongPlayer from "../components/SongPlayer";
 import PlayingView from "../components/PlayingView";
 import NowPlayingContext from "../context/NowPlayingProvider";
 import PropType from "prop-types";
+import MobileNavBar from "../components/MobileNavBar";
+import useIsMobile from "../hooks/useIsMobile";
 
 const DashboardLayout = ({ children }) => {
   const { playingView } = useContext(NowPlayingContext);
+  const isMobile = useIsMobile();
   return (
     <div className="h-screen relative">
-      <div className="flex h-full bg-black p-4 pb-0">
-        {/* Sidebar component */}
-        <Sidebar />
+      <div className={`flex h-full bg-black pb-0 ${!isMobile && "p-4"}`}>
+        {!isMobile && <Sidebar />}
         <main
-          className={`flex-grow bg-gradient-to-t from-black to-sky-700 rounded-lg p-4 overflow-y-scroll mb-20  ${
+          className={`flex-grow bg-gradient-to-t from-black to-sky-700 ${
+            !isMobile && "rounded-lg"
+          } p-4 overflow-y-scroll ${
             playingView
               ? "lg:w-3/4 md:w-2/3 lg:block md:block hidden"
               : " block"
           }`}
         >
-          {/* Content */}
-          {/* Main content */}
-          {children}
+          <div className={`${isMobile ? "mb-44" : "mb-20"}`}>{children}</div>
         </main>
         {playingView && (
-          <section className="lg:w-1/4 md:w-1/3 w-full bg-black p-4 text-white overflow-y-scroll mb-20 overflow-x-hidden">
+          <section
+            className={`lg:w-1/4 md:w-1/3 w-full bg-black p-4 text-white overflow-y-scroll ${
+              isMobile ? "mb-44" : "mb-20"
+            } overflow-x-hidden`}
+          >
             <PlayingView />
           </section>
         )}
       </div>
-
-      <section className="bg-black p-2 absolute bottom-0 w-full">
-        <div className=" h-full">
-          {/* song player */}
-          <SongPlayer />
-        </div>
+      <section
+        className={`${
+          isMobile ? "bg-slate-900/70 backdrop-blur-sm" : "bg-black"
+        }  absolute bottom-0 w-full rounded-lg`}
+      >
+        <SongPlayer />
+        {isMobile && <MobileNavBar />}
       </section>
     </div>
   );

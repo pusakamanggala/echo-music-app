8920ba628459cdfc63eeb6b258538c20a4baf922
Pusaka Manggala <pusakamanggala@gmail.com>
2023-09-02 15:08:35 +0700

    add : mobile nav bar

---
+++ src/components/MobileNavBar.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faMagnifyingGlass,
+  faCircleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+
+const MobileNavBar = () => {
+  const [showNote, setShowNote] = useState(false);
+  return (
+    <nav className="grid grid-cols-3 items-center p-5 bg-slate-900">
+      <NavLink
+        to="/browse"
+        title="Browse"
+        className={({ isActive }) =>
+          isActive
+            ? "text-white flex flex-col justify-center items-center"
+            : "text-gray-500 flex flex-col justify-center items-center"
+        }
+      >
+        <FontAwesomeIcon className="h-6" icon={faMagnifyingGlass} />
+        <p className="text-xs font-semibold">Search</p>
+      </NavLink>
+      <NavLink
+        title="Home"
+        to="/home"
+        className={({ isActive }) =>
+          isActive
+            ? "text-white flex flex-col justify-center items-center"
+            : "text-gray-500 flex flex-col justify-center items-center"
+        }
+      >
+        <FontAwesomeIcon className="h-6" icon={faHouse} />
+        <p className="text-xs font-semibold">Home</p>
+      </NavLink>
+      <div className=" flex justify-center">
+        <button
+          type="button"
+          title="Note"
+          className={` h-5 flex relative ${
+            showNote ? "text-green-700" : "text-white animate-pulse"
+          } `}
+          onClick={() => setShowNote(!showNote)}
+        >
+          <FontAwesomeIcon icon={faCircleExclamation} className="h-6" />
+          {showNote && (
+            <h1 className="text-xs m-2 absolute bottom-4 right-2 bg-white/60 text-black backdrop-blur-md w-80 p-2 rounded-md font-semibold text-justify">
+              While you can interact with the player&apos;s controls, it
+              won&apos;t play actual songs due to technical constraints. For an
+              immersive music streaming experience, it is recommended to use
+              official Spotify music streaming platforms.
+            </h1>
+          )}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default MobileNavBar;

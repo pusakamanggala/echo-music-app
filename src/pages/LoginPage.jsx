78d097855d3ca7152f3e4e36eb10fc3e0d9e5465
Pusaka Manggala <pusakamanggala@gmail.com>
2023-09-13 21:09:19 +0700

    add : animated background and app screenshot

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import useLogin from "../hooks/useLogin";
+import Screenshot from "../img/screenshot.png";
 
 const LoginPage = () => {
   const clientId = "4c028baed0074747ab50de3ba9ac86b9";
@@ -17,15 +18,42 @@ const LoginPage = () => {
     window.location.reload();
   }
   return (
-    <div className="h-dvh bg-sky-600 flex justify-center items-center">
-      <button
-        type="button"
-        title="Login"
-        className="text-white p-3 rounded-lg bg-green-600 shadow-md"
-        onClick={handleLogin}
-      >
-        Login with Spotify
-      </button>
+    <div className="gradient-background h-dvh md:grid md:grid-cols-2 ">
+      <div className="h-full overflow-hidden md:block hidden ">
+        <img
+          src={Screenshot}
+          className="object-cover object-right h-full"
+          alt=""
+        />
+      </div>
+
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-white font-semibold text-2xl md:text-4xl lg:text-5xl xl:text-8xl">
+          Welcome
+        </h1>
+        <p className="text-white mb-8 text-base md:text-xl lg:text-2xl xl:text-4xl">
+          to <span className="font-semibold">Echo</span> |{" "}
+          <span className="text-green-500 font-semibold">Spotify</span> Clone
+        </p>
+        <button
+          type="button"
+          title="Login"
+          className="text-white text-sm md:text-base lg:text-xl p-3 rounded-2xl bg-green-600 hover:bg-green-700 shadow-md"
+          onClick={handleLogin}
+        >
+          Take me in !
+        </button>
+      </div>
+      <footer className="absolute bottom-5 right-5 text-white">
+        Made with &lt;3 by{" "}
+        <a
+          href="https://pusakamanggala.netlify.app/"
+          target="_blank"
+          className="hover:underline"
+        >
+          Pusaka Manggala
+        </a>
+      </footer>
     </div>
   );
 };

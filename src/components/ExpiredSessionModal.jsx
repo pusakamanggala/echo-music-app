71439e77bb74c317e794ef21e89f6c098712f863
Pusaka Manggala <pusakamanggala@gmail.com>
2023-09-13 21:08:35 +0700

    add : expired session modal

---
+++ src/components/ExpiredSessionModal.jsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import { getAccessTokenFromCookie } from "../utils/helpers";
+
+const ExpiredSessionModal = () => {
+  const accessToken = getAccessTokenFromCookie();
+
+  console.log(accessToken);
+
+  return (
+    accessToken === undefined && (
+      <div className="fixed inset-0 flex flex-col space-y-4 items-center justify-center z-50 h-dvh w-screen bg-black/75 bg-opacity-30">
+        <div className="h-4/6 w-4/6 relative overflow-hidden flex items-center justify-center">
+          <div className="absolute text-white md:text-4xl text-xl font-semibold text-center mx-8">
+            <h1>Sorry your session has ended</h1>
+            <h1>please reload the page and re-enter</h1>
+          </div>
+          <a
+            className="text-base absolute bottom-5 text-white right-5 hover:underline"
+            href="https://developer.spotify.com/documentation/web-api/concepts/rate-limits"
+            target="_blank"
+          >
+            Learn more
+          </a>
+          <img
+            src="https://wallpapercrafter.com/th800/15664-glass-drops-wet-rain-transparent-moody-window-4k.jpg"
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </div>
+    )
+  );
+};
+
+export default ExpiredSessionModal;
